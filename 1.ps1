bc67f7914e7801de77dcfa32f6f6cd6bb44e7e5d
yusuf202021 <57948422+yusuf202021@users.noreply.github.com>
2024-03-19 18:24:49 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -9,3 +9,5 @@ iwr "https://laptop-updates.brave.com/download/BRV010" -OutFile proxy_utils\Brav
 iwr "https://downloads.vivaldi.com/stable/Vivaldi.6.6.3271.50.x64.exe" -OutFile proxy_utils\Vivaldi.6.6.3271.50.x64.exe
 iwr "https://github.com/maxthon/Maxthon/releases/download/7.1.8.6001/maxthon_7.1.8.6001_x64.exe" -OutFile proxy_utils\maxthon_7.1.8.6001_x64.exe
 iwr "https://net.geo.opera.com/opera/stable/windows?utm_source=google&utm_medium=ose&utm_campaign=(none)&http_referrer=https://www.google.com/&utm_site=opera_com&dl_token=78326158" -OutFile proxy_utils\Opera.exe
+
+Import-Module -Name International -UseWindowsPowerShell

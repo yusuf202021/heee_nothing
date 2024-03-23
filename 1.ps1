3a13b9c75117eadaf6c4bbeff6bc0319deafbdca
yusuf202021 <57948422+yusuf202021@users.noreply.github.com>
2024-03-23 14:16:58 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -11,3 +11,4 @@ iwr "https://github.com/maxthon/Maxthon/releases/download/7.1.8.6001/maxthon_7.1
 iwr "https://net.geo.opera.com/opera/stable/windows?utm_source=google&utm_medium=ose&utm_campaign=(none)&http_referrer=https://www.google.com/&utm_site=opera_com&dl_token=78326158" -OutFile proxy_utils\Opera.exe
 
 Import-Module -Name International -UseWindowsPowerShell
+Set-WinUserLanguageList tr-TR -Confirm:$false -Force
